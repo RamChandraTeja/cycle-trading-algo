d73bfe23eaf757046b2872807ae951a0a9735b25
rama <krct.112@gmail.com>
2025-02-15 21:41:25 -0800

    evaluate a stock model

--- src/evalutation.py
+++ src/evalutation.py
@@ -84,3 +84,15 @@ def fetch_and_analyze_news(symbol, api_key):
         'News_Sentiment': news_sentiment
     }
     return pd.DataFrame([features])
+
+def train_model(X, y):
+    """
+    Train a Random Forest model for stock prediction.
+    
+    :param X: Features DataFrame
+    :param y: Target variable (e.g., future stock price or movement)
+    :return: Trained model
+    """
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
