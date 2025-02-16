6eb6d956d780cab04c32915e6ca311ad0439b1a9
rama <krct.112@gmail.com>
2025-02-15 21:41:41 -0800

    evaluate a stock model

--- src/evalutation.py
+++ src/evalutation.py
@@ -96,3 +96,8 @@ def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model = RandomForestRegressor(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
+
+    # Evaluate model
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
